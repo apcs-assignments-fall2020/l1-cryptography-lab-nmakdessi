87d43aa2ed68a6ed28b4f5c6a6e6eab7c6547a6d
nmakdessi <21nmakdessi@athenian.org>
2020-09-21 20:42:25 +0000

    Finished assignment

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -2,23 +2,99 @@ import java.util.Scanner;
 
 public class Caesar {
     public static String encryptCaesar(String message) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        int shift = 3;
+        int adjusted = 0;
+        String encrypted = "";
+        for(int i = 0; i < message.length(); i++){
+            if (message.charAt(i) >= 'A' && message.charAt(i) <= 'Z'){
+                adjusted = ((int)(message.charAt(i)) + shift - 'A') % 26;
+                encrypted += (char)((int) 'A' + adjusted);
+            }
+            else if (message.charAt(i) >= 'a' && message.charAt(i) <= 'z'){
+                adjusted = ((int)(message.charAt(i)) + shift - 'a') % 26;
+                encrypted += (char)((int) 'a' + adjusted);
+            }
+            else{
+                encrypted += message.charAt(i);
+            }
+            
+        }
+        return encrypted;
     }
 
     public static String decryptCaesar(String message) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        int shift = 3;
+        int adjusted = 0;
+        String encrypted = "";
+        for(int i = 0; i < message.length(); i++){
+            if (message.charAt(i) >= 'A' && message.charAt(i) <= 'Z'){
+                adjusted = ((int)(message.charAt(i)) - shift - 'A') % 26;
+                if (adjusted < 0) {
+                    adjusted = 26 + adjusted;
+                }
+                encrypted += (char)((int) 'A' + adjusted);
+            }
+            else if (message.charAt(i) >= 'a' && message.charAt(i) <= 'z'){
+                adjusted = ((int)(message.charAt(i)) - shift - 'a') % 26;
+                if (adjusted < 0) {
+                    adjusted = 26 + adjusted;
+                }
+                encrypted += (char)((int) 'a' + adjusted);
+            }
+            else{
+                encrypted += message.charAt(i);
+            }
+            
+        }
+        return encrypted;
     }
 
     public static String encryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        int shift = key;
+        int adjusted = 0;
+        String encrypted = "";
+        for(int i = 0; i < message.length(); i++){
+            if (message.charAt(i) >= 'A' && message.charAt(i) <= 'Z'){
+                adjusted = ((int)(message.charAt(i)) + shift - 'A') % 26;
+                encrypted += (char)((int) 'A' + adjusted);
+            }
+            else if (message.charAt(i) >= 'a' && message.charAt(i) <= 'z'){
+                adjusted = ((int)(message.charAt(i)) + shift - 'a') % 26;
+                encrypted += (char)((int) 'a' + adjusted);
+            }
+            else{
+                encrypted += message.charAt(i);
+            }
+            
+        }
+        return encrypted;
     }
 
     public static String decryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        int shift = key;
+        int adjusted = 0;
+        String encrypted = "";
+        for(int i = 0; i < message.length(); i++){
+            if (message.charAt(i) >= 'A' && message.charAt(i) <= 'Z'){
+                adjusted = ((int)(message.charAt(i)) - shift - 'A') % 26;
+                if (adjusted < 0) {
+                    adjusted = 26 + adjusted;
+                }
+                encrypted += (char)((int) 'A' + adjusted);
+            }
+            else if (message.charAt(i) >= 'a' && message.charAt(i) <= 'z'){
+                adjusted = ((int)(message.charAt(i)) - shift - 'a') % 26;
+                if (adjusted < 0) {
+                    adjusted = 26 + adjusted;
+                }
+                encrypted += (char)((int) 'a' + adjusted);
+            }
+            else{
+                encrypted += message.charAt(i);
+            }
+            
+        }
+        return encrypted;
     }
 
 
